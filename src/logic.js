cc14286bbc977b86258777189bd18cba8ef779fe
Timurlexx <timurlexx@gmail.com>
2023-05-28 18:42:46 +0600

    update

--- src/logic.js
+++ src/logic.js
@@ -13,33 +13,75 @@ export async function initCommand(ctx) {
 
 export async function processTextToChat(ctx, content) {
   try {
+    // Добавляем сообщение пользователя в сессию
     ctx.session.messages.push({ role: openai.roles.USER, content });
+
+    // Получаем ответ от OpenAI GPT
     const response = await openai.chat(ctx.session.messages);
 
+    // Добавляем ответ от GPT в сессию
     ctx.session.messages.push({
       role: openai.roles.ASSISTANT,
       content: response.content,
     });
 
-    const language = franc(content);
+    // Определяем язык введенного текста с помощью библиотеки franc
+    const language = franc(content, { minLength: 3, whitelist: ['eng', 'rus', 'fra', 'spa', 'ita', 'deu', 'zho', 'jpn', 'kor', 'ara', 'por', 'nld', 'tur', 'hin', 'ind', 'ben'] });
     let audioOptions;
 
-    if (language === 'rus') {
-      audioOptions = {
-        lang: 'ru',
-        slow: false,
-        speed: 2,
-        voice: 'ru-RU-Wavenet-D',
-      };
-    } else {
-      audioOptions = {
-        lang: 'en',
-        slow: false,
-        speed: 2,
-        voice: 'en-US-Wavenet-D',
-      };
+    // Устанавливаем параметры для озвучки текста
+    switch (language) {
+      case 'rus':
+        audioOptions = { lang: 'ru', slow: false, speed: 2, voice: 'ru-RU-Wavenet-D' }; // Russian voice
+        break;
+      case 'fra':
+        audioOptions = { lang: 'fr', slow: false, speed: 2, voice: 'fr-FR-Wavenet-D' }; // French voice
+        break;
+      case 'spa':
+        audioOptions = { lang: 'es', slow: false, speed: 2, voice: 'es-ES-Wavenet-D' }; // Spanish voice
+        break;
+      case 'ita':
+        audioOptions = { lang: 'it', slow: false, speed: 2, voice: 'it-IT-Wavenet-D' }; // Italian voice
+        break;
+      case 'deu':
+        audioOptions = { lang: 'de', slow: false, speed: 2, voice: 'de-DE-Wavenet-D' }; // German voice
+        break;
+      // Additional languages
+      case 'zho':
+        audioOptions = { lang: 'zh-CN', slow: false, speed: 2, voice: 'cmn-CN-Wavenet-D' }; // Simplified Chinese voice
+        break;
+      case 'jpn':
+        audioOptions = { lang: 'ja', slow: false, speed: 2, voice: 'ja-JP-Wavenet-D' }; // Japanese voice
+        break;
+      case 'kor':
+        audioOptions = { lang: 'ko', slow: false, speed: 2, voice: 'ko-KR-Wavenet-D' }; // Korean voice
+        break;
+      case 'ara':
+        audioOptions = { lang: 'ar', slow: false, speed: 2, voice: 'ar-XA-Wavenet-D' }; // Arabic voice
+        break;
+      case 'por':
+        audioOptions = { lang: 'pt', slow: false, speed: 2, voice: 'pt-BR-Wavenet-D' }; // Portuguese voice
+        break;
+      case 'nld':
+        audioOptions = { lang: 'nl', slow: false, speed: 2, voice: 'nl-NL-Wavenet-D' }; // Dutch voice
+        break;
+      case 'tur':
+        audioOptions = { lang: 'tr', slow: false, speed: 2, voice: 'tr-TR-Wavenet-D' }; // Turkish voice
+        break;
+      case 'hin':
+        audioOptions = { lang: 'hi', slow: false, speed: 2, voice: 'hi-IN-Wavenet-D' }; // Hindi voice
+        break;
+      case 'ind':
+        audioOptions = { lang: 'id', slow: false, speed: 2, voice: 'id-ID-Wavenet-D' }; // Indonesian voice
+        break;
+      case 'ben':
+        audioOptions = { lang: 'bn', slow: false, speed: 2, voice: 'bn-BD-Wavenet-D' }; // Bengali voice
+        break;
+      default:
+        audioOptions = { lang: 'en', slow: false, speed: 2, voice: 'en-US-Wavenet-D' }; // Default to English voice
     }
 
+    // Получаем аудио-данные с помощью Google TTS API
     const audioData = await gtts.getAllAudioBase64(response.content, audioOptions);
 
     // Отправка текстового сообщения пользователю
--- src/main.js
+++ src/main.js
@@ -7,45 +7,64 @@ import { openai } from './openai.js';
 import { removeFile } from './utils.js';
 import { initCommand, processTextToChat, INITIAL_SESSION } from './logic.js';
 
+// Создание экземпляра бота с использованием токена Telegram из конфигурации
 const bot = new Telegraf(config.get('TELEGRAM_TOKEN'));
 
+// Использование сессий для хранения данных о сеансе с каждым пользователем
 bot.use(session());
 
+// Обработчик команды /new
 bot.command('new', initCommand);
 
+// Обработчик команды /start
 bot.command('start', initCommand);
 
+// Обработчик голосовых сообщений
 bot.on(message('voice'), async (ctx) => {
   ctx.session ??= INITIAL_SESSION;
   try {
     await ctx.reply(code('Сообщение принял. Жду ответ от сервера...'));
+
+    // Получение ссылки на файл голосового сообщения
     const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id);
     const userId = String(ctx.message.from.id);
+
+    // Создание ogg-файла из голосового сообщения
     const oggPath = await ogg.create(link.href, userId);
+
+    // Преобразование ogg-файла в mp3
     const mp3Path = await ogg.toMp3(oggPath, userId);
 
+    // Удаление временного ogg-файла
     removeFile(oggPath);
 
+    // Получение текстовой транскрипции из mp3-файла с помощью OpenAI
     const text = await openai.transcription(mp3Path);
     await ctx.reply(code(`Ваш запрос: ${text}`));
 
+    // Обработка текстовой транскрипции
     await processTextToChat(ctx, text);
   } catch (e) {
     console.log(`Ошибка при голосовом сообщении`, e.message);
   }
 });
 
+// Обработчик текстовых сообщений
 bot.on(message('text'), async (ctx) => {
   ctx.session ??= INITIAL_SESSION;
   try {
     await ctx.reply(code('Сообщение отправлено. Ждем ответ от сервера...'));
+
+    // Обработка текстового сообщения
     await processTextToChat(ctx, ctx.message.text);
   } catch (e) {
     console.log(`Ошибка при текстовом сообщении`, e.message);
   }
 });
 
+// Запуск бота
 bot.launch();
 
+// Обработка сигналов SIGINT и SIGTERM для остановки бота
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
